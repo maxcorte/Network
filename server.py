c886e4efe67f9c4c500fb247ed30443735abd80d
maxcorte <maxime.delcorte@student.uclouvain.be>
2026-03-02 21:10:29 +0100

    correction trame

--- server.py
+++ server.py
@@ -10,25 +10,28 @@ def encode(type, window, seqnum, timestamp, payload):
         case "PTYPE_ACK": type_bits = 2
         case "PTYPE_SACK": type_bits = 3
         case _: type_bits = 0
-    byte1 = struct.pack('!B', (type_bits << 6 | window))
-    header.append(byte1)
 
     length = len(payload)
-    header.append(struct.pack('!H', length))
-    header.append(struct.pack('!H', seqnum))
+
+    word = (type_bits << 30) | (window << 24) | (length << 11) | seqnum
+
+    header.append(struct.pack('!I', word))
     header.append(struct.pack('I', timestamp))
 
     header_bytes = b''.join(header)
-    crc1 = zlib.crc32(header_bytes)
+    crc1 = struct.pack('!I', zlib.crc32(header_bytes) & 0xffffffff)
 
     message.append(header_bytes)
-    message.append(struct.pack('!I',crc1))
+    message.append(crc1)
     message.append(payload)
     if payload:
-        crc2 = zlib.crc32(payload)
-        message.append(struct.pack('!I', crc2))
+        crc2 = struct.pack('!I', zlib.crc32(payload) & 0xffffffff)
+        message.append(crc2)
     full_message = b''.join(message)
     return full_message
+
+if __name__ == "__main__":
+    print(encode("PTYPE_DATA", 5, 10, 1234567890, b"Hello"))
 
 
 
